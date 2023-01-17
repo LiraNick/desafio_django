bc313e314ecd7594bbfdc3257f75b07e77ed92b3
Nicolas Lira Galiano <nicolas.galiano@proway.treina>
2023-01-16 21:56:44 -0300

    django em andamento

---
+++ aplicacao/alunos/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    alunos = {
+       1: 'Lirinha',
+       2: 'Felipe',
+       3: 'Marcos'
+   }
+
+    dados = {
+       'nome_do_aluno' : alunos
+   }
+
+    return render(request,'index.html', dados)
+    
+def aluno(request):
+    return render(request,'aluno.html')
